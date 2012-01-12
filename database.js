145b65aee152100d0e9569f94d3c98f32c38451b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-01-12 20:09:48 +0200

    Provided ugly api

--- database.js
+++ database.js
@@ -5,16 +5,19 @@ function init() {
     return db;
 }
 
-function createTable( db, table ) {
+function createTable( db, table, structure ) {
     db.transaction( function(tx) {
-         tx.executeSql('CREATE TABLE IF NOT EXISTS ' + table )
-    } )
+                       joined = structure.join( ',' );
+                       joined = '(' + joined + ')';
+                       tx.executeSql('CREATE TABLE IF NOT EXISTS ' + table + ' ' + joined )
+                   } );
 }
 
 function select( db, table, callback ) {
     db.transaction( function(tx) {
-         results = tx.executeSql('SELECT * FROM ' + table);
-    } )
+                       results = tx.executeSql('SELECT * FROM ' + table);
+                       callback( results );
+                   } );
 }
 
 function insert( db , table, values ) {
@@ -22,5 +25,5 @@ function insert( db , table, values ) {
                        joined = values.join( ',' );
                        joined = '(' + joined + ')';
                        tx.executeSql('INSERT INTO ' + table + ' VALUES ' + joined );
-    } )
+                   } );
 }
