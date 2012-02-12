fa685428de717f85aabda77dce8343da06603c10
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-02-12 21:24:15 +0200

    Initial wrapperfuntion around the SQL library

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ function init() {
 
 function createTable( db, table, structure ) {
     db.transaction( function(tx) {
-                       joined = structure.join( ',' );
+                       var joined = structure.join( ',' );
                        joined = '(' + joined + ')';
                        tx.executeSql('CREATE TABLE IF NOT EXISTS ' + table + ' ' + joined )
                    } );
@@ -15,15 +15,15 @@ function createTable( db, table, structure ) {
 
 function select( db, table, callback ) {
     db.transaction( function(tx) {
-                       results = tx.executeSql('SELECT * FROM ' + table);
+                       var results = tx.executeSql('SELECT * FROM ' + table);
                        callback( results );
                    } );
 }
 
 function insert( db , table, values ) {
     db.transaction( function(tx) {
-                       joined = values.join( ',' );
+                       var joined = '?, ?';
                        joined = '(' + joined + ')';
-                       tx.executeSql('INSERT INTO ' + table + ' VALUES ' + joined );
+                       tx.executeSql('INSERT INTO ' + table + ' VALUES'  + joined, values );
                    } );
 }
